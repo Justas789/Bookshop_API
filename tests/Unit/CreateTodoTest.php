40de95fa35da9165331b742c1d6c11331f90b55e
Justas789 <nba.justas@gmail.com>
2023-04-03 19:59:10 +0300

    first commit

--- tests/Unit/CreateTodoTest.php
+++ tests/Unit/CreateTodoTest.php
@@ -12,98 +12,34 @@ $data = [
 
  
 ];
-
- 
- 
-
- 
 $this->post(route('posts.store'), $data)
-
- 
 ->assertStatus(201)
-
- 
-->assertJson($data);
-
- 
+->assertJson($data); 
 }
-
- 
- 
-
- 
 public function test_can_update_post() {
-
- 
- 
-
- 
 $post = factory(Post::class)->create();
-
- 
- 
-
- 
-$data = [
-
- 
+$data = [ 
 'title' => $this->faker->sentence,
-
- 
 'content' => $this->faker->paragraph
 
  
 ];
 
- 
- 
-
- 
 $this->put(route('posts.update', $post->id), $data)
-
- 
 ->assertStatus(200)
-
- 
 ->assertJson($data);
 
- 
 }
 
- 
- 
-
- 
 public function test_can_show_post() {
-
- 
- 
-
- 
 $post = factory(Post::class)->create();
-
- 
- 
-
- 
 $this->get(route('posts.show', $post->id))
-
- 
 ->assertStatus(200);
 
- 
 }
 
- 
- 
 
- 
 public function test_can_delete_post() {
-
- 
- 
-
- 
 $post = factory(Post::class)->create();
 
  
@@ -111,8 +47,6 @@ $post = factory(Post::class)->create();
 
  
 $this->delete(route('posts.delete', $post->id))
-
- 
 ->assertStatus(204);
 
  
@@ -123,14 +57,8 @@ $this->delete(route('posts.delete', $post->id))
 
  
 public function test_can_list_posts() {
-
- 
 $posts = factory(Post::class, 2)->create()->map(function ($post) {
-
- 
 return $post->only(['id', 'title', 'content']);
-
- 
 });
 
  
@@ -138,14 +66,8 @@ return $post->only(['id', 'title', 'content']);
 
  
 $this->get(route('posts'))
-
- 
 ->assertStatus(200)
-
- 
 ->assertJson($posts->toArray())
-
- 
 ->assertJsonStructure([
 
  
